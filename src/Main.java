08eb4f655b769d2541362594b434636497af960d
zz0-0ss <zohre.soorani1780@gmail.com>
2022-12-24 00:14:03 +0330

    Creating an object from the graphics page in the main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainPage mainPage = new MainPage();
+                mainPage.setVisible(true);
+            }
+        });
     }
 }
